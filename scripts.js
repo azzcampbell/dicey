68fb69166147d303f92514507a6891d2a34f5882
Gurkage <azz.campbell@gmail.com>
2021-01-08 17:11:28 +0000

    Small updates

--- scripts.js
+++ scripts.js
@@ -6,19 +6,15 @@ function randomNumber() {
   var a = random() // First random number
   var b = random() // Second random number
 
-  var playerOneDice = `images/dice${a}.png` // Update the SRC path with the first variable
-  document.querySelectorAll("img")[0].src = playerOneDice // Update Player 1's dice image
-
-  var playerTwoDice = `images/dice${b}.png` // Update the SRC path with the first variable
-  document.querySelectorAll("img")[1].src = playerTwoDice // Update Player 2's dice image
-
-  document.querySelector("h1").style.fontSize = "6rem"
+  document.querySelectorAll("img")[0].src = `images/dice${a}.png` // Target the image source and replace it
+  document.querySelectorAll("img")[1].src = `images/dice${b}.png` // Target the image source and replace it
+  document.querySelector("h1").style.fontSize = '6rem' // Reduce the font size once the button is pressed
 
   if (b > a) {
     document.querySelector("h1").innerText = 'Player 2 Wins!'
   } else if (a > b) {
     document.querySelector("h1").innerText = 'Player 1 Wins!'
   } else {
-    document.querySelector("h1").innerText = "Draw!"
+    document.querySelector("h1").innerText = 'Draw!'
   }
 }
