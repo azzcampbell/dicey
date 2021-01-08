bc3364ba551e23a49f284049a82afb8a7e07a96e
Gurkage <azz.campbell@gmail.com>
2021-01-08 15:10:55 +0000

    Bug fixes

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
-function randomNumber() {
-    var randomNumber1 = Math.floor((Math.random() * 6) + 1)
-    var randomNumber2 = Math.floor((Math.random() * 6) + 1)
-    var dicePlayer1 = "images/dice" + randomNumber1 + ".png"
-    var dicePlayer2 = `images/dice${randomNumber2}.png`
-    document.querySelectorAll("img")[0].src = dicePlayer1
-    document.querySelectorAll("img")[1].src = dicePlayer2
+function random() {
+  return Math.floor((Math.random() * 6) + 1) // Round down a random number between 0 and 1, multiply it by 6 and then add 1
+}
 
-    if (randomNumber1 === randomNumber2) {
-      document.querySelector("h1").innerText = "Draw!"
-      document.querySelector("h1").style.fontSize = "6rem"
-    } else if (randomNumber1 > randomNumber2) {
-      document.querySelector("h1").innerHTML = '<img src="images/flag.png" class="flag"> Player 1 Wins!</img>'
-      document.querySelector("h1").style.fontSize = "6rem"
-    } else if (randomNumber1 < randomNumber2) {
-      document.querySelector("h1").innerHTML = ' Player 2 Wins! <img src="images/flag.png" class="flag"></img>'
-      document.querySelector("h1").style.fontSize = "6rem"
-    }
+function randomNumber() {
+  var a = random() // First random number
+  var b = random() // Second random number
+
+  var playerOneDice = `images/dice${a}.png` // Update the SRC path with the first variable
+  document.querySelectorAll("img")[0].src = playerOneDice // Update Player 1's dice image
+
+  var playerTwoDice = `images/dice${b}.png` // Update the SRC path with the first variable
+  document.querySelectorAll("img")[1].src = playerTwoDice // Update Player 2's dice image
+
+  document.querySelector("h1").style.fontSize = "6rem"
+
+  if (b > a) {
+    document.querySelector("h1").innerText = 'Player 2 Wins!'
+  } else if (a > b) {
+    document.querySelector("h1").innerText = 'Player 1 Wins!'
+  } else {
+    document.querySelector("h1").innerText = "Draw!"
   }
+}
